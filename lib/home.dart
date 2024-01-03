1ab5f507e6a6ba49ccc92ff0801bf2de0dabe183
ahnaf217 <ahnaflabib56@gmail.com>
2024-01-04 04:28:43 +0600

    Added route from homepage to statistics page.

--- lib/home.dart
+++ lib/home.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-class HomePage extends StatelessWidget{
+import 'statistics.dart';
+import 'bottomnavigationbar.dart';
+
+class HomePage extends StatelessWidget {
   var height, width;
 
   List imgData = [
     "assets/chat 2.jpg",
-        "assets/stat 2.jpg",
-        "assets/transaction 2.jpg",
-        "assets/woman.png"
+    "assets/stat 2.jpg",
+    "assets/transaction 2.jpg",
+    "assets/woman.png"
   ];
 
   List titles = [
@@ -16,39 +19,39 @@ class HomePage extends StatelessWidget{
     "Transaction",
     "Profile",
   ];
+
   HomePage({super.key});
   final user = FirebaseAuth.instance.currentUser!;
-  //sign user out
-  void signUserOut(){
-    try{
-      FirebaseAuth.instance.signOut();
-    }on FirebaseAuthException catch(e){
 
-    }
+  // sign user out
+  void signUserOut() {
+    try {
+      FirebaseAuth.instance.signOut();
+    } on FirebaseAuthException catch (e) {}
   }
+
   @override
   Widget build(BuildContext context) {
     height = MediaQuery.of(context).size.height;
     width = MediaQuery.of(context).size.width;
     return Scaffold(
-
       body: Container(
-          color: Colors.amber,
+        color: Colors.amber,
         child: Column(
           children: [
             Container(
               decoration: BoxDecoration(),
               height: height * 0.25,
-              width : width,
+              width: width,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
-                      padding: EdgeInsets.only(
-                    top: 36,
-                    left: 20,
-                    right: 20,
-                  ),
+                    padding: EdgeInsets.only(
+                      top: 36,
+                      left: 20,
+                      right: 20,
+                    ),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
@@ -67,19 +70,16 @@ class HomePage extends StatelessWidget{
                             borderRadius: BorderRadius.circular(15),
                             color: Colors.white,
                             image: DecorationImage(
-                              image: AssetImage("assets/woman.png",)
-                            ),
+                                image: AssetImage("assets/woman.png")),
                           ),
                         )
                       ],
                     ),
-
                   ),
                   Padding(
                     padding: EdgeInsets.only(
                       top: 20,
                       left: 30,
-
                     ),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -101,30 +101,40 @@ class HomePage extends StatelessWidget{
             ),
             Container(
               decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                )
-              ),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  )),
               height: height * 0.75,
               width: width,
               padding: EdgeInsets.symmetric(vertical: 60),
               child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 1.1,
-                    mainAxisSpacing: 25,
-                  ),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 1.1,
+                  mainAxisSpacing: 25,
+                ),
                 shrinkWrap: true,
                 physics: NeverScrollableScrollPhysics(),
                 itemCount: imgData.length,
-                itemBuilder: (context, index){
-                    return InkWell(
-                      onTap: () {},
-                      child: Container(
-                        margin: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-                        decoration: BoxDecoration(
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {
+                      if (index == 1) {
+                        // Check if the Statistics container is tapped
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Bottom(),
+                          ),
+                        );
+                      }
+                    },
+                    child: Container(
+                      margin:
+                      EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+                      decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(20),
                           color: Colors.white,
                           boxShadow: [
@@ -132,37 +142,32 @@ class HomePage extends StatelessWidget{
                               color: Colors.black26,
                               spreadRadius: 1,
                               blurRadius: 6,
-
                             )
-                          ]
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Image.asset(imgData[index],
+                          ]),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Image.asset(
+                            imgData[index],
                             width: 100,
+                          ),
+                          Text(
+                            titles[index],
+                            style: TextStyle(
+                              fontSize: 21,
+                              fontWeight: FontWeight.bold,
                             ),
-                            Text(
-                              titles[index],
-                              style: TextStyle(
-                                fontSize: 21,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            )
-                          ],
-                        ),
+                          )
+                        ],
                       ),
-                    );
-                }
+                    ),
+                  );
+                },
               ),
             )
           ],
         ),
-      ), // Row
-
-
-
-      // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
